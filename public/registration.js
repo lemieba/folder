79076d10da60b119a74e1aab1796cdf9e8f1fb3d
OumarKaraga <oumarkaraga@Oumars-MacBook-Pro.local>
2016-11-05 20:31:25 -0400

    sign in and sign out page

--- public/registration.js
+++ public/registration.js
@@ -1,40 +1,69 @@
 $(document).ready(function() {
-  $("#register").click(function() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var cpassword = $("#cpassword").val();
+    var databaseRef = new Firebase("https://basic-add1a.firebaseio.com/");
 
-    // // Get a reference to the database service
-    //  var database = firebase.database();
-    // var writeUserData = function(userId, name, email, password) {
-    //   firebase.database().ref('users/' + userId).set({
-    //     username: name,
-    //     email: email,
-    //     password : password,
-    //   });
-    // };
+    $("#save").click(collectData);
+    
+    /*$("#sign-in").click(function(){
+          var email = document.getElementById("email").value;
+          var password = document.getElementById("password").value;
+          window.location.href = "homepage.html";
+    });
+     */
 
-    if (name == '' || email == '' || password == '' || cpassword == '') {
-      alert("Please fill all fields...!!!!!!");
-    } else if ((password.length) < 8) {
-      alert("Password should atleast 8 character in length...!!!!!!");
-    } else if (!(password).match(cpassword)) {
-      alert("Your passwords don't match. Try again?");
-    } else {
-      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+    function collectData(){
+      var firtsname = document.getElementById("firstname").value;
+      var lastname = document.getElementById("lastname").value;
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+      var email = document.getElementById("email").value;
+
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+        console.log('success');
+
+        
+        var database = firebase.database();
+        // var user = firebase.auth().currentUser;
+        firebase.database().ref('users/' + user.uid).set({
+          username: username,
+          email: email,
+        });
+        databaseRef.child("users").push({firstname:firstname, lastname: lastname}); 
+
+     window.location.href = "homepage.html";
+      },function(error) {
+     // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+       if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+       }
+      else {
         alert(errorMessage);
+      }
+      console.log(error);
+       
+    });
+   }
+
+
+    /*
+    function logIn(){
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+      //window.location.href = "homepage.html";
+
+
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // 
       });
-      var database = firebase.database();
-      var user = firebase.auth().currentUser;
-      firebase.database().ref('users/' + user.uid).set({
-        username: name,
-        email: email,
-      });
-        //  $("form")[0].reset();
+      window.location.href = "homepage.html";
+
     }
-  });
+   */
+    
+
 });
